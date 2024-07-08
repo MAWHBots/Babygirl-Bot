7ce4f26307d99f09e211db666b3aef87e6232713
MAWHBots <160210473+MAWHBots@users.noreply.github.com>
2024-07-08 12:57:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,27 +15,27 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '23323912'))
 API_HASH = environ.get('API_HASH', '5b7decd292e78a57d631ea1849b7098a')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7294576495:AAECCKSx67pYhQNbasUpYdmaxnFzKf6UmbI')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6318243977 5324831370 6444128136').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5324831370 6444128136').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/OrzzQueryBot")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002202300892'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+MsHpvs2TFlpjZTY1')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002099257639 -1001943771138').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://aayushraj0281:UDgE6cYzPVNGlV0W@cluster0.ht9qb6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+DATABASE_URI = environ.get('DATABASE_URI', "")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002216649502'))
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', ''))
 QR_CODE = environ.get('QR_CODE', 'https://telegra.ph/file/52e5b3f0103d997ffbedb.jpg')
 START_IMG = environ.get('START_IMG', 'https://telegra.ph/file/47f1bd32b9ad6628789d1.jpg')
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL','-1002026291321'))
-DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002212409575'))
+DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS',''))
 URL = environ.get('URL', 'adambot-b1919552bb8e.herokuapp.com/')
 STICKERS_IDS = ('CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME').split()
 FILE_AUTO_DEL_TIMER = int(environ.get('FILE_AUTO_DEL_TIMER', '600'))
 IS_VERIFY = is_enabled('IS_VERIFY', True)
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002086571200'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', ''))
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/Movie_Url_link_downloader/17")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
 SHORTENER_API = environ.get("SHORTENER_API", "49ff923da91ec127edca47fe22d4d2d8d7514669")
@@ -53,12 +53,12 @@ YEARS = [f'{i}' for i in range(2024 , 2002,-1 )]
 SEASONS = [f'season {i}'for i in range (1 , 23)]
 REF_PREMIUM = 10
 PREMIUM_POINT = 1000
-auth_channel = environ.get('AUTH_CHANNEL', '-1002110905372')
+auth_channel = environ.get('AUTH_CHANNEL', '')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002230094777'))
-request_channel = environ.get('REQUEST_CHANNEL', '-1002244588420')
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', ''))
+request_channel = environ.get('REQUEST_CHANNEL', '')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
-UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002207296876'))
+UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', ''))
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002025104764'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
